df139e9f81c1f9952bd55507235b07119592ef23
陈海飞 <15378702772@163.com>
2020-03-03 22:20:46 +0800

    update exe

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,13 @@
 import { app, BrowserWindow } from 'electron'
 
+require('update-electron-app')({
+  repo: 'https://github.com/SeakeyCode/electronvue.git', // github存储库
+  updateInterval: '5 minutes', // 检查更新的频率
+  logger: require('electron-log'), // 定义log功能的自定义记录器对象
+  notifyUser:true , //启用后，将提示用户下载后立即应用更新。
+})
+
+
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
